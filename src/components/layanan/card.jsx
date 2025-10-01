dcbc0b91eed6b7a8f5c43d3a8401b2faeebe53e5
klyroe <keylaanmrr@gmail.com>
2025-10-01 14:39:22 +0700

    fix(card): icon visibility fixed

--- src/components/layanan/card.jsx
+++ src/components/layanan/card.jsx
@@ -4,9 +4,9 @@ const MenuCards = () => {
 
   const FeatureCard = ({ title, desc, icon }) => {
   return (
-    <div className="p-17 shadow-[3.12px_9.37px_21.20px_0_rgba(0,0,0,0.03)] rounded-2xl bg-white flex flex-col items-start pl-10 gap-3 border-2 border-primary-500">
+    <div className="px-17 shadow-[3.12px_9.37px_21.20px_0_rgba(0,0,0,0.03)] rounded-2xl bg-white flex flex-col items-start pl-10 gap-3 border-2 py-15 border-primary-500 cursor-pointer transition-transform duration-300 ease-in-out hover:-translate-y-1 hover:shadow-lg">
       <img src={icon} alt={title} className="w-12 h-12" />
-      <h3 className="text-lg font-semibold text-left">{title}</h3>
+      <h3 className="text-2xl text-primary-500 font-bold leading-[24px] text-left">{title}</h3>
       <p className="text-sm text-gray-500 text-left">{desc}</p>
     </div>
   );
@@ -16,27 +16,27 @@ const MenuCards = () => {
     {
       title: "Deteksi Wajah",
       desc: "Teknologi pendeteksi wajah yang membantu mengenali ekspresi emosional Anda untuk memberikan saran dan dukungan yang lebih tepat.",
-      icon: "/public/assets/icons/deteksiWajah.svg",
+      icon: "/assets/icons/deteksiWajah.svg",
     },
     {
       title: "Jurnal Pintar",
       desc: "Catat perasaan dan pengalaman harian Anda menggunakan jurnal digital dengan analisis berbasis NLP untuk memahami pola emosi secara mendalam.",
-      icon: "/public/assets/icons/jurnalPintar.svg",
+      icon: "/assets/icons/jurnalPintar.svg",
     },
     {
       title: "Visualisasi",
       desc: "Tempat untuk melihat seluruh riwayat dan hasil dari Face Detection, Smart Journaling, hingga aktivitas lainnya secara visual sehingga memudahkan Anda memantau perkembangan kondisi mental.",
-      icon: "/public/assets/icons/visualisasi.svg",
+      icon: "/assets/icons/visualisasi.svg",
     },
     {
       title: "Obrolan Anonim",
       desc: "Sesi obrolan anonim dengan pendamping atau komunitas untuk berbagi cerita, mengurangi beban, dan merasa didengar tanpa rasa khawatir.",
-      icon: "/public/assets/icons/obrolanAnonim.svg",
+      icon: "/assets/icons/obrolanAnonim.svg",
     },
     {
       title: "Meditasi",
       desc: "Koleksi musik-musik menenangkan yang dirancang untuk membantu mengurangi stres, meningkatkan fokus, dan menciptakan suasana rileks saat meditasi.",
-      icon: "/public/assets/icons/meditasi.svg",
+      icon: "/assets/icons/meditasi.svg",
     },
   ];
 
