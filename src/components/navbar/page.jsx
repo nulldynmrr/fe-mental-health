d1707bca78e12e27e433fdde175b51686b543be1
nulldynmrr <dinarmuhammadakbar04@gmail.com>
2025-10-04 21:27:29 +0700

    fix: format import navbar component

--- src/components/navbar/page.jsx
+++ src/components/navbar/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Button from "@/components/button/page";
-import { useRouter} from "next/navigation";
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
   const router = useRouter();
