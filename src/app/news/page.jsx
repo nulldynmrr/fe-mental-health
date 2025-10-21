1ea60d1986de13adbb661470e8bd47c36a09ca4a
klyroe <keylaanmrr@gmail.com>
2025-10-21 14:29:34 +0700

    feat: slicing ui (raw)

---
+++ src/app/news/page.jsx
@@ -0,0 +1,30 @@
+import Navbar from "@/components/navbar/page";
+import Breadcrumb from "@/components/breadcrumb/page";
+import React from "react";
+import NewsCard from "@/components/berita/card";
+import Berita from "@/components/news/upper";
+
+const NewsPage = () => {
+    return (
+        <div>
+            <Navbar/>
+            <div className="p-6 md:px-20 md:py-12">
+            <Breadcrumb
+            items={[
+                { label: "Dashboard", href: "/dashboard" },
+                { label: "Berita" },
+            ]}
+            />
+            </div>
+
+            <Berita/>
+
+            <br/>
+            <h1>Berita Lainnya</h1>
+            <NewsCard/>
+
+        </div>
+    );
+};
+
+export default NewsPage;
